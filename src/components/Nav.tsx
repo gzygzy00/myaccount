e78c0c88de37a201919cf5add3ab5f014b433e2b
gzy <283211498@qq.com>
2021-09-29 01:44:04 +0800

    改进 Nav 组件，配置 svg-loader

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -11,12 +11,21 @@ const NavWrapper = styled.nav`
     display: flex;
 
     > li {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
       width: 33.33%;
       text-align: center;
-      padding: 4px 0;
+
+      > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 4px 0;
+
+        &.selected {
+          color: #FFF;
+          fill: #fff;
+          background-color: chocolate;
+        }
+      }
 
       .icon {
         width: 24px;
@@ -31,16 +40,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name={'money'}/>
-          <Link to="/money">记账页</Link>
+          <NavLink to="/money" activeClassName="selected">
+            <Icon name={'money'}/>
+            记账页
+          </NavLink>
         </li>
         <li>
-          <Icon name={'tag'}/>
-          <Link to="/tags">标签页</Link>
+          <NavLink to="/tags" activeClassName="selected">
+            <Icon name={'tag'}/>
+            标签页
+          </NavLink>
         </li>
         <li>
-          <Icon name={'chart'}/>
-          <Link to="/statistics">统计页</Link>
+          <NavLink to="/statistics" activeClassName="selected">
+            <Icon name={'chart'}/>
+            统计页
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
