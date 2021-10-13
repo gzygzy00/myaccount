314e406aef385e0a2806033eb3b99e688c9c7a1d
gzy <283211498@qq.com>
2021-10-13 21:45:24 +0800

    初步添加Money页标签

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,9 +1,61 @@
+import styled from 'styled-components';
 import Layout from '../components/Layout';
 
+const TagSection = styled.section`
+	border: 1px solid red;
+`;
+
+const NotesSection = styled.section`
+	border: 1px solid red;
+`;
+
+const CategorySection = styled.section`
+	border: 1px solid red;
+`;
+
+const NumberPadSection = styled.section`
+	border: 1px solid red;
+`;
+
 export default function Money() {
-  return (
-    <Layout>
-      Money
-    </Layout>
-  );
+	return (
+		<Layout>
+			Money
+			<TagSection>
+				<ol>
+					<li>衣</li>
+					<li>食</li>
+					<li>住</li>
+					<li>行</li>
+				</ol>
+				<button>新增标签</button>
+			</TagSection>
+			<NotesSection>
+        <label>
+          <span>备注</span>
+          <input type="text" />
+        </label>
+      </NotesSection>
+			<CategorySection>
+				<ul>
+					<li>支出</li>
+					<li>收入</li>
+				</ul>
+			</CategorySection>
+			<NumberPadSection>
+				<div>100</div>
+				<div>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+					<button>1</button>
+				</div>
+			</NumberPadSection>
+		</Layout>
+	);
 }
